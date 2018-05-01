458f8c2d40a117f2757d47b1fdd4ad67f753d231
Jeff Chen <>
2018-05-01 00:13:18 -0700

    committing

--- app/components/Form/Form.jsx
+++ app/components/Form/Form.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import TextField from 'material-ui/TextField';
+import { CardForm } from 'react-payment';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class Form extends React.Component {
   constructor(props) {
     super(props)
   }
 
+  onSubmit(card) {
+    const { number, exp_month, exp_year, cvc, name, zip } = card;
+  }
   
   render() {
     return(
-      <div>
-        <TextField
-        hintText="Hint Text"
-        /><br />
-      </div>
+      <MuiThemeProvider>
+        <CardForm
+          onSubmit={this.onSubmit}
+          getName={true}
+          getZip={true}
+        />
+      </MuiThemeProvider>
     )
   }
 }
